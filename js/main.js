caeef9b867ee409718baa2a7ed827aeab880ce9e
Ibrahim-MohamedH <Ibrahim20.Mohamed@gmail.com>
2022-10-08 11:23:48 +0200

    fun v1.3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
 
 let timing = new Date().getTime();
 timing = new Date(timing);
-let hours = timing.toString().substring(16, 18);
-let minutes = timing.toString().substring(19, 21);
-let seconds = timing.toString().substring(22, 24);
-
-timeTillSR = `${05 - hours}:${56 - minutes}:${60 - seconds}`;
-let span = document.querySelector("span");
-span.textContent = timeTillSR;
+console.log(new Date().getHours())
+let hours = new Date().getHours();
+let minutes = new Date().getMinutes();
+let seconds = new Date().getSeconds();
+let h1 = document.querySelector(".main-time")
+let question = document.querySelector(".q")
+let answer = document.querySelector(".a")
 let button = document.querySelector("button");
+let timeTillSR = `${05 - hours}:${56 - minutes}:${60 - seconds}`;
+if(hours <= 17 &&  hours > 5){
+  if((34 - minutes) < 0){
+    timeTillSR = `${17 - hours}:${minutes - 34}:${60 - seconds}`;
+    h1.innerHTML = `باقي على غروب الشمس <br> <span>0<span> <br>  ساعة فقط`
+    let span = document.querySelector("span");
+    span.textContent = timeTillSR;
+    question.textContent = "بجد؟"
+    answer.textContent = "مفيش بجنيه ثقة؟"
+  }else{
+    timeTillSR = `${17 - hours}:${34 - minutes}:${60 - seconds}`;
+    h1.innerHTML = `باقي على غروب الشمس <br> <span>0<span> <br>  ساعة فقط`
+    let span = document.querySelector("span");
+    span.textContent = timeTillSR;
+    question.textContent = "بجد؟"
+    answer.textContent = "مفيش بجنيه ثقة؟🤦‍♂️ "
+  }
+}else{
+  if(hours < 24){
+    timeTillSR = `${hours - 05}:${56 - minutes}:${60 - seconds}`;
+    let span = document.querySelector("span");
+    span.textContent = timeTillSR;
+  }else{
+    let span = document.querySelector("span");
+    span.textContent = timeTillSR;
+  }
+}
 // ##############################################
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -19,10 +46,7 @@ async function displayNone(q, t) {
   await sleep(1000);
   t.classList.remove("none");
 }
-
 // #######################################
-question = document.querySelector(".q")
-answer = document.querySelector(".a")
 
 button.addEventListener("click", async () => {
   location.reload();
